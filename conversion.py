80022ad27627edfb6b63e4f39c0bb8ea75ae8b06
Periklis <pericles.petr@gmail.com>
2018-03-09 14:33:08 +0000

    Added km to miles function

--- conversion.py
+++ conversion.py
@@ -2,3 +2,13 @@ def dollars2cents(dollars):
     cents = dollars * 100
     return cents
 
+
+
+
+
+#My conversion function is better than yours
+
+def km2miles(km)
+	miles=km*0.621
+	return miles
+ 
